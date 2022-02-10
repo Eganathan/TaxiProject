463090d3519c18b593f203891860368f87fbd50c
Eganathan R <ammasamrith@gmail.com>
2022-02-10 22:32:42 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,11 @@
 package playground;
 
-import java.util.Date;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
+
 		new LoadData();
 		new App();
-		//Data.customerData();
-		//Data.carData();
-		//Data.routeData();
-		
-		//Data.newTrip();
 	}
-	
 
 }
